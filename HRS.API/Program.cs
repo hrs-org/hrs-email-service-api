32d7bd09a6cb2174e8e91102a5db776e6767e150
shenxurui83-cpu <shenxurui83@gmail.com>
2025-10-16 14:05:13 +0800

    dockerfile

--- HRS.API/Program.cs
+++ HRS.API/Program.cs
@@ -53,27 +53,9 @@ builder.Services.AddSwaggerGen(c =>
     });
 });
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 
 builder.Services.AddAutoMapper(cfg => { }, typeof(Program));
-
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer = builder.Configuration["Jwt:Issuer"],
-            ValidAudience = builder.Configuration["Jwt:Audience"],
-            IssuerSigningKey = new SymmetricSecurityKey(
-                Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!)
-            )
-        };
-    });
 
 builder.Services.AddCors(options =>
 {
@@ -91,15 +73,18 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseSwagger();
-if (app.Environment.IsDevelopment()) app.UseSwaggerUI();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "HRS API v1");
+    c.RoutePrefix = "swagger";
+});
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors("AllowWebClient");
-app.UseAuthentication();
-app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
+
